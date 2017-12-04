4df7de576830e9021ccff88403347dba2b6ea165
zhang <zhang@qq.com>
2017-12-04 20:25:28 +0800

    轮播图

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # carousel
 广告轮播图
+
+carousel1 用动画功能实现图片的滚动
+
+carousel2 move.js实现图片的切换，实现原理，改通过定时器改变图片的z-index 
+
+carousel3 用面向对象的方法实现图片的轮播
---
+++ carousel2/js/move.js
@@ -0,0 +1,35 @@
+		function starMove(obj,json,fn){
+
+			clearInterval(obj.timer);
+
+			obj.timer = setInterval(function(){
+				var bStop = true;
+				for(attr in json){
+					var curr = parseFloat(getComputedStyle(obj,false)[attr]);
+					
+					if(attr == 'opacity'){
+						// alert("asfjk");
+						curr = Math.round(curr*100);
+					}
+					
+					var speed = (json[attr] - curr)/10;
+					
+					speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+
+					if(json[attr] != curr){
+						bStop =false;
+						if(attr == "opacity"){
+							obj.style[attr] = (curr + speed)/100;
+						}else{
+							obj.style[attr] = curr + speed + "px";
+						}
+					}
+					if(bStop){
+						clearInterval(obj.timer);
+						if(fn){
+							fn();
+						}
+					}	
+				}
+			},30);
+		}
